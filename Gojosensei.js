b636c6ec76d977e27878a8856c74518f1821fe8c
eslam3011 <116306189+eslam3011@users.noreply.github.com>
2022-11-12 11:36:56 +0200

    Update Gojosensei.js

--- Gojosensei.js
+++ Gojosensei.js
@@ -1,4 +1,4 @@
-دا ظهرrequire('./settings')
+require('./settings')
 const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require('@adiwajshing/baileys')
 const fs = require('fs')
 const util = require('util')
