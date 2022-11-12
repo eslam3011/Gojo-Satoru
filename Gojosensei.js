e70155d1f6311be2d5f2081410dfac1a63f8da42
eslam3011 <116306189+eslam3011@users.noreply.github.com>
2022-11-12 12:54:38 +0200

    Update Gojosensei.js

--- Gojosensei.js
+++ Gojosensei.js
@@ -1196,7 +1196,7 @@ GojoMdNx.sendMessage(from, { text: `Question : What ${q}\nAnswer : ${kahk}` }, {
 					break
 case 'من':
 				if (!text) return replay(`Use Text, Example : ${prefix + command} you fuck her lol `)
-					const bisa = [`الغبي`,`الذكي't`,`المحترم`,`Of Course You Can!!!`]
+					const bisa = [`الغبي`,`الذكي'`,`المحترم`,`الشاذ]
 					const ga = bisa[Math.floor(Math.random() * bisa.length)]
 GojoMdNx.sendMessage(from, { text: `Question : من ${q}\nAnswer : ${ga}` }, { quoted: m })
 
@@ -1229,8 +1229,8 @@ case 'ذكاء':
 GojoMdNx.sendMessage(from, { text: `*${command}*\n\nNama : ${q}\nAnswer : *${tik}%*` }, { quoted: m })
 
 					break
-case 'awesomecheck':
-  case 'greatcheck':
+case 'حبي لي':
+  case 'كرهي لي:
     case 'gaycheck':
       case 'cutecheck':
         case 'lesbicheck':
@@ -1239,7 +1239,7 @@ case 'awesomecheck':
                  case 'prettycheck':
                     case 'lovelycheck':
                       case 'uglycheck':
-				if (!text) return replay(`Tag Someone, Example : ${prefix + command} @Xeon`)
+				if (!text) return replay(`االشخص  النسبة : ${prefix + command} @Xeon`)
 					const sangeh = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
 					const sange = sangeh[Math.floor(Math.random() * sangeh.length)]
 GojoMdNx.sendMessage(from, { text: `*${command}*\n\nName : ${q}\nAnswer : *${sange}%*` }, { quoted: m })
